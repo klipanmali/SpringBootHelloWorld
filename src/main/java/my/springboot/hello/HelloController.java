9483c7860959978c2e7677109a324003bd28a198
T H <>
2019-09-25 20:47:50 +0200

    Logging
    Logging and Logback configuration via xml file

--- src/main/java/my/springboot/hello/HelloController.java
+++ src/main/java/my/springboot/hello/HelloController.java
@@ -1,5 +1,7 @@
 package my.springboot.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(HelloController.class);
+
+	// assign value defined in spring.application.name if exist
+	// or assign default name "demoservice"
 	@Value("${spring.application.name:demoservice}")
 	private String appName;
 
 	@RequestMapping("/")
 	public String index() {
+		logger.debug("You have hit 'Greetings' page");
 		return "Greetings from " + appName + " Spring Boot!";
 	}
 

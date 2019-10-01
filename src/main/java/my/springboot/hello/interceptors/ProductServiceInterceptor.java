bfe5b3f391066ec1877e25449dbffaac8a91181b
T H <>
2019-10-01 07:05:11 +0200

    Interceptors
    operations before sending the request to the controler
    operations before sending response to the client
    operations after completing the request and response

---
+++ src/main/java/my/springboot/hello/interceptors/ProductServiceInterceptor.java
@@ -0,0 +1,47 @@
+package my.springboot.hello.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import my.springboot.hello.ApplicationStartupRunner;
+
+@Component
+public class ProductServiceInterceptor implements HandlerInterceptor {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(ApplicationStartupRunner.class);
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		logger.info("Interceptor done afthe completion");
+		HandlerInterceptor.super.afterCompletion(request, response, handler,
+				ex);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		logger.info(
+				"Interceptor done some things before sending request to the Contrlorer");
+		HandlerInterceptor.super.postHandle(request, response, handler,
+				modelAndView);
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		logger.info(
+				"Interceptor done some things before sending response to the client");
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+}
